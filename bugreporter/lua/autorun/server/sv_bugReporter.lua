3e1fabc79af70b932b133af1e53db969babcd5ab
Jake <SubjectAlpha@users.noreply.github.com>
2016-12-24 13:32:28 -0500

    Update sv_bugReporter.lua

--- bugreporter/lua/autorun/server/sv_bugReporter.lua
+++ bugreporter/lua/autorun/server/sv_bugReporter.lua
@@ -9,6 +9,10 @@ local DATABASE_PASSWORD = "pass"
 
 local BugDB = tmysql.Connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME, DATABASE_PORTS)
 
+hook.Add("Initialize", "create_bugdb", function()
+	BugDB:Query("CREATE TABLE IF NOT EXISTS bug_report (BugReport VARCHAR(500), SteamID VARCHAR(20));")
+end)
+
 local function GetSteamID(len, ply)
     local bug_report_value = net.ReadString()
     BugDB:Query( "INSERT INTO bug_report (BugReport, SteamID) VALUES ('" .. BugDB:Escape(bug_report_value) .. "','" .. ply:SteamID() .. "');")
