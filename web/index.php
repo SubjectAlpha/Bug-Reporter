d999525691db5f088d262f61c656e3d2aadf0db2
Jake <SubjectAlpha@users.noreply.github.com>
2016-12-24 19:28:17 -0500

    Moar creditz

--- web/index.php
+++ web/index.php
@@ -55,5 +55,6 @@
 <footer>
     <p>&copy;2016 <a href="https://github.com/SubjectAlpha" style="color:#fff;">Subject_Alpha</a> (Main idea + Basic Structure)</p>
     <p><a href="https://github.com/Lunaversitay/Bug-Reporter/tree/master/web" style="color:#fff;">Lunaversity</a> (Styles + Web Improvements)</p>
+    <p><a href="https://github.com/meharryp" style="color:#fff;">meharryp</a> (Security improvements)</p>
 </footer>
 </html>
