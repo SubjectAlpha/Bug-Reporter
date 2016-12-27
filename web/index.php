92507a77ca9d87332c88244d24013dee53b9a956
Jake <SubjectAlpha@users.noreply.github.com>
2016-12-26 22:25:09 -0500

    Switch to PDO + better style

--- web/index.php
+++ web/index.php
@@ -6,53 +6,62 @@
     $DB_NAME = "generic_name";
 //END CONFIG--------------------------
 
-    $connection = new mysqli($DB_HOSTNAME, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
-    $result = mysqli_query($connection, "SELECT SteamID,BugReport FROM bug_report");
-    mysqli_fetch_assoc($result);
-    mysqli_close($connection); //Make sure to close out the database connection
+    $pdo = new PDO("mysql:host=$DB_HOSTNAME;dbname=$DB_NAME", $DB_USERNAME, $DB_PASSWORD);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$result = array();
+	try {
+   		$result = $pdo->query("SELECT SteamID, BugReport FROM bug_report");
+    	$result = $result->fetchAll();
+	} catch (Exception $e) {
+    	$error = $e->getMessage();
+	}
 ?>
 
 <!DOCTYPE HTML>
 <html>
-<head>
-    <title>3FX Bug Reports</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
+	<head>
+    	<title>Bug Reports</title>
+    	<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/darkly/bootstrap.min.css">
+    	<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
 
-<body>
-    <div class="center-page">
-    <div>
-    <tbody>
-    <div class="table">
-        <table>
-            <tr>
-                <td colspan="2">Bug Reports</td>
-            </tr>
-            <tr>
-                <td>SteamID</td>
-                <td>Description</td>
-            </tr>
-                <?php foreach($result as $bu=>$b): ?>
-            <tr>
-            <td>
-                <?php echo htmlspecialchars($b['SteamID'], ENT_QUOTES, 'UTF-8'); ?>
-            </td>
-            <td>
-                <?php echo htmlspecialchars($b['BugReport'], ENT_QUOTES, 'UTF-8'); ?>
-            </td>
-            </tr>
-            <?php endforeach; ?>
-        </table>
-    </div>
-    </tbody>
-    </div>
-    </div>
-</body>
+	<body class="background-color">
+    	<div class="container">
+        	<div class="row">
+           		<div class="col-lg-10 col-lg-push-1">
+                	<?php
+                		if (isset($error)) {
+                    		echo "<div class='alert alert-danger'>$error</div>";
+                		} else {
+                	?>
+                    	<table class="table table-bordered">
+                        	<thead>
+                            	<tr>
+                                	<th>Reporter (SteamID)</th>
+                                	<th>Description</th>
+                            	</tr>
+                        	</thead>
 
-<footer>
-    <p>&copy; <?php echo date("Y") ?> by <a href="https://github.com/SubjectAlpha">Subject_Alpha</a></p>
-</footer>
+                        	<tbody>
+                            	<?php
+                            		foreach ($result as $row) {
+                                		echo "<tr><td>{$row["SteamID"]}</td><td>{$row["BugReport"]}</td></tr>";
+                            		}
+                            	?>
+                        	</tbody>
+                    	</table>
+
+                    	<?php 
+                    		} 
+                    	?>
+            	</div>
+        	</div>
+    	</div>
+	</body>
+    <footer>
+        &copy; <?php echo date("Y") ?> by <a href="https://github.com/SubjectAlpha">Subject_Alpha</a>
+    </footer>
 </html>
