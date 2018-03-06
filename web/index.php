68e8772c213cb3bb266ea82c53432ecb2e65da9b
Jake Starr <SubjectAlpha@users.noreply.github.com>
2018-03-06 09:32:24 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -25,7 +25,7 @@
     	<meta charset="UTF-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-    	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/darkly/bootstrap.min.css">
+    	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     	<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 
